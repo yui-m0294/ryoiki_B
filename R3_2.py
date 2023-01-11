5bc57cb0418ae06aaaf2e92a271ac71f7ba567f3
yui-m0294 <84368393+yui-m0294@users.noreply.github.com>
2023-01-11 10:36:25 +0900

    Add files via upload

---
+++ R3_2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 11:43:37 2022
+
+@author: mm082
+"""
+
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry('700x700')
+canvas = tk.Canvas(root, height=700, width=700)
+
+canvas.create_rectangle(225, 75, 275, 100,fill='red',outline='red',tag="id1")
+canvas.create_rectangle(200,100,300,135,fill='red',outline='red',tag="id1")
+canvas.create_oval(205,135,225,155,fill='black',tag="id1")
+canvas.create_oval(275,135,295,155,fill='black',tag="id1")
+
+canvas.pack()
+
+
+def key_event(e):
+   key = e.keysym
+   x, y = 0, 0
+   if key == "Up":
+       y -= 10
+   if key == "Down":
+       y += 10
+   if key == "Left":
+       x -= 10
+   if key == "Right":
+       x += 10
+       
+   canvas.move("id1", x, y)
+
+
+root.bind("<KeyPress>", key_event)
+
+root.mainloop()
