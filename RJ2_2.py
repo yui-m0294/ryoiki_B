e4ff1436e5813e0d97d60f5dd21b3eea1cb0836e
yui-m0294 <84368393+yui-m0294@users.noreply.github.com>
2023-01-11 10:38:41 +0900

    Update RJ2_2.py

--- RJ2_2.py
+++ RJ2_2.py
@@ -7,9 +7,7 @@ def draw_car(x,y,scale,color):
     canvas.create_rectangle(x+10*scale, y+15*scale, x+25*scale, y+20*scale,fill=color,outline=color)
     canvas.create_oval(x+11*scale,y+20*scale,x+15*scale,y+24*scale,fill='black')
     canvas.create_oval(x+20*scale,y+20*scale,x+24*scale,y+24*scale,fill='black')
-
-
-
+    
 for i in range(10):
     for j in range(10):
         if(i+j)%2==0:
@@ -19,5 +17,3 @@ for i in range(10):
         draw_car(50*j,50*i,3,color)
 canvas.pack()
 root.mainloop()
-    
-    
